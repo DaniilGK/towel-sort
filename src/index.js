1ddb984e02a46d483b6bf2fe4a42a9159752be32
DaniilGOK <gerasimenokd638@gmail.com>
2022-02-16 00:24:16 +0300

    feat: comlite towel-sort tastk

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+       if(matrix != null) {
+         for(let i = 0; i < matrix.length; i++) {
+           if(i % 2 == 0) {for(let e of matrix[i]) {result.push(e)}}
+                    else {for(let j of matrix[i].reverse()) {result.push(j)}}
+         }
+     }
+   return result 
 }
